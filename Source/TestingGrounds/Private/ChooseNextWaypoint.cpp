d9c08458b0dbbd8024f6fef23605365df5b6b83e
fengsi <fengsi727@qq.com>
2019-01-14 14:50:22 +0800

    Create a PatrolRoute Actor Component and attach it to the third person characters. (Use composition instead of inheritance)

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,18 +1,24 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// Get the patrol points
-	auto AIController =  OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointCPP;
+	// Get the patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
 
 	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
---
+++ Source/TestingGrounds/Public/PatrolRoute.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolRoute.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TESTINGGROUNDS_API UPatrolRoute : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	TArray<AActor*> GetPatrolPoints() const;
+
+private:
+	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+		TArray<AActor*> PatrolPoints;
+};
